67c519202a39dc712b5d6f6e2c89879c6c556893
Yogii <yogii@Yogiis-MacBook-Pro.local>
2026-01-30 14:16:31 +0530

    Clear repository

--- middleware/rateLimiter.js
+++
@@ -1,8 +0,0 @@
-const rateLimits = {
-  general: (req, res, next) => next(),
-  auth: (req, res, next) => next(),
-  kyc: (req, res, next) => next(),
-  payout: (req, res, next) => next()
-};
-
-module.exports = rateLimits;
